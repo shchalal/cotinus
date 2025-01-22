1b87f0439aa6ee4c92c111ba28fba00789df7cda
shchalal <shchalal@mail.ru>
2025-01-22 16:35:44 +0300

    версия 2.0

--- my_scripts.js
+++ my_scripts.js
@@ -1,11 +1,10 @@
-<script>
+document.addEventListener('DOMContentLoaded', function () {
+    const burgerMenu = document.querySelector('.hamburger');
+    const menu = document.querySelector('.menu_header');
 
-    document.addEventListener('DOMContentLoaded', function () {
-        const burgerMenu = document.querySelector('.hamburger');
-        const menu = document.querySelector('.menu_header');
+    burgerMenu.addEventListener('click', function () {
+        menu.classList.toggle('active');
+        burgerMenu.classList.toggle('active')
 
-        burgerMenu.addEventListener('click', function () {
-            menu.classList.toggle('active');
         });
     });
-</script>
